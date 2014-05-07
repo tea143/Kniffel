d4e3e92cb3f14534dfbcbe98b8d896104a5c6b60
tea143 <ualbersmeier@stud.hs-bremen.de>
2014-05-07 15:15:05 +0200

    First Commit

---
+++ main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: ASUS
+ *
+ * Created on 7. Mai 2014, 15:10
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
