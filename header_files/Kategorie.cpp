cd624f314e660a007cabe587a2ee253c470e2c25
lstaroverova <l_staroverova@mail.ru>
2014-05-24 11:29:09 +0200

    Delete

--- header_files/Kategorie.cpp
+++
@@ -1,3 +0,0 @@
-#include "kategorie.h"
-
-
--- main.cpp
+++ main.cpp
@@ -1,12 +1,11 @@
-#include <iostream>
-#include "Spielfeld.h"
-#include "Spieler.h"
-#include "EinAusgabe.h"
+#include "header_files/spielfeld.h"
+#include "header_files/spieler.h"
+#include "header_files/einAusgabe.h"
 
 using namespace std;
 
 int main() {
-    cout << "Das Kniffel-Spiel faengt an:\n" << endl;
+    cout << "Das Kniffel-Spiel fängt an:\n" << endl;
     cout << "Wie viele Spieler spielen mit? " << endl;
     EinAusgabe io;
     size_t spielerAnzahl = io.readNumberBetween(1,5);
